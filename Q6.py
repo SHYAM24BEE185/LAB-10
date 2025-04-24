27d66d600095167ed3c46058e6f30cdad7e96d28
Shyam24BEE185 <lunaiscool1706@gmail.com>
2025-04-24 19:06:05 +0530

    Q6.py

--- Q6.py
+++ Q6.py
@@ -1,10 +1,10 @@
 with open ('file1.txt','w+') as f1:
-    f1.write("I am in file1\nIt is text1\nIt is text2")
+    f1.write("this is in file1\nIt is text1\nIt is text2")
     f1.seek(0)
     ch1=f1.readlines()
 
 with open ('file2.txt','w+') as f2:
-    f2.write("I am in file2\nIt is text1\nIt is text2")
+    f2.write("this is in file2\nIt is text1\nIt is text2")
     f2.seek(0)
     ch2=f2.readlines()
 
